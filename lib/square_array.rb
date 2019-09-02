d9bc297ccfb26b3f5dcdb3e70c07bd88e80233da
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-02 22:29:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   ary = Array.new
   while counter < array.length do
-    puts ary << array[counter]
+    puts ary << array[counter] ** 2
     counter += 1
   end
   return ary
